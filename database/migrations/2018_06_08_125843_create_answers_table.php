b70868059a3b5cb3d9af3f0fdee6b1c5906269c3
kalii8 <liva.karlivane@gmail.com>
2018-06-08 15:19:40 +0200

    Database

---
+++ database/migrations/2018_06_08_125843_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('question_id')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->text('text')->nullable();
+            $table->timestamps(); //creates  created_at and updated_at automatically
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
