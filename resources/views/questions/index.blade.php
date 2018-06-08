d69c48151c8767fb2a30038812bd7d20f30adb08
kalii8 <liva.karlivane@gmail.com>
2018-06-08 14:10:43 +0200

    adding questions

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -31,6 +31,49 @@
                 </div>
             </div>
         </div>
+
+         <div class="question">
+            <div class="question-left">
+                <div class="question-stat">
+                    <span>7</span>
+                    <label>responses</label>
+                </div>
+                <div class="question-stat">
+                    <span>16</span>
+                    <label>votes</label>
+                </div>
+            </div>
+            <div class="question-right">
+                <div class="question-name">
+                    <a href="{{route('show question',['id' => 2])}}">I don't know how to Laravel????!!!?</a>
+                </div>
+                <div class="question-info">
+                    asked at 2017-03-03 14:23:22 by <a href="">slavo</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="question">
+            <div class="question-left">
+                <div class="question-stat">
+                    <span>1</span>
+                    <label>responses</label>
+                </div>
+                <div class="question-stat">
+                    <span>14</span>
+                    <label>votes</label>
+                </div>
+            </div>
+            <div class="question-right">
+                <div class="question-name">
+                    <a href="{{route('show question',['id' => 3])}}">Is orange or lemon better for breakfast?</a>
+                </div>
+                <div class="question-info">
+                    asked at 2017-03-03 14:23:22 by <a href="">slavo</a>
+                </div>
+            </div>
+        </div>
+
     </div>
 </section>
 
